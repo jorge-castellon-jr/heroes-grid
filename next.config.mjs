434b6cc111b427964e797bbac99483d786635296
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-04-19 14:12:09 -0700

    Adds libsql/client to next js config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  experimental: {
+    // Add @libsql/client here
+    serverExternalPackages: ["@libsql/client"],
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
