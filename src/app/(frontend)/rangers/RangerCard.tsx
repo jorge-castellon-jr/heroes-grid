c6c97c112020f322e57c9200d51045cf9952213d
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-04-19 13:52:49 -0700

    Fixes css react type issue

--- src/app/(frontend)/rangers/RangerCard.tsx
+++ src/app/(frontend)/rangers/RangerCard.tsx
@@ -5,22 +5,22 @@ import { Ranger } from '@/payload-types';
 // Helper function or object to map color names to CSS-friendly values
 // Add all your colors from the interface here!
 const rangerColorStyles: Record<Ranger['color'], React.CSSProperties> = {
-  red: { '--ranger-color': '#E23730', '--text-color': '#FFFFFF' },
-  blue: { '--ranger-color': '#0078F0', '--text-color': '#FFFFFF' },
-  black: { '--ranger-color': '#222222', '--text-color': '#FFFFFF' },
-  yellow: { '--ranger-color': '#FFDE00', '--text-color': '#333333' }, // Darker text for yellow
-  pink: { '--ranger-color': '#F991C8', '--text-color': '#333333' }, // Darker text for pink
-  green: { '--ranger-color': '#009A4E', '--text-color': '#FFFFFF' },
-  white: { '--ranger-color': '#FAFAFA', '--text-color': '#333333' }, // Darker text for white
-  gold: { '--ranger-color': '#D4AF37', '--text-color': '#333333' },
-  silver: { '--ranger-color': '#C0C0C0', '--text-color': '#333333' },
-  shadow: { '--ranger-color': '#4A0404', '--text-color': '#FFFFFF' },
-  crimson: { '--ranger-color': '#990000', '--text-color': '#FFFFFF' },
-  navy: { '--ranger-color': '#000080', '--text-color': '#FFFFFF' },
-  orange: { '--ranger-color': '#FFA500', '--text-color': '#333333' },
-  purple: { '--ranger-color': '#800080', '--text-color': '#FFFFFF' },
-  zenith: { '--ranger-color': '#ADA9EC', '--text-color': '#333333' },
-  dark: { '--ranger-color': '#1C1C1C', '--text-color': '#FFFFFF' },
+  red: { '--ranger-color': '#E23730', },
+  blue: { '--ranger-color': '#0078F0', },
+  black: { '--ranger-color': '#222222', },
+  yellow: { '--ranger-color': '#FFDE00' }, // Darker text for yellow
+  pink: { '--ranger-color': '#F991C8' }, // Darker text for pink
+  green: { '--ranger-color': '#009A4E' },
+  white: { '--ranger-color': '#FAFAFA' }, // Darker text for white
+  gold: { '--ranger-color': '#D4AF37' },
+  silver: { '--ranger-color': '#C0C0C0' },
+  shadow: { '--ranger-color': '#4A0404' },
+  crimson: { '--ranger-color': '#990000' },
+  navy: { '--ranger-color': '#000080' },
+  orange: { '--ranger-color': '#FFA500' },
+  purple: { '--ranger-color': '#800080' },
+  zenith: { '--ranger-color': '#ADA9EC' },
+  dark: { '--ranger-color': '#1C1C1C' },
 };
 
 interface RangerCardProps {
