f18468537c6be0af313d32b8ed483f493c768824
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-04-19 14:34:04 -0700

    use pnpm to build

--- src/app/(frontend)/cards/page.tsx
+++ src/app/(frontend)/cards/page.tsx
@@ -27,7 +27,6 @@ export default async function CardsPage() {
       <h1>Card Library</h1>
       {cards.length > 0 ? (
         <div className="cards-grid">
-          {' '}
           {/* Reusing the grid class or create a new one */}
           {cards.map((card) => (
             <CardDisplay key={card.id} card={card} />
