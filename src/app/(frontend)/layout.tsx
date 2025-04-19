baf566d2299ab2a16cff5c799f3f359769be7ab3
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-04-19 15:08:28 -0700

    Updates meta title and desctription

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './styles.css'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: 'Heroes Grid - Companion app to Power Rangers: Heroes of the Grid',
+  title: 'Heroes Grid',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
